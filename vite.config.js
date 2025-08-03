6fd52466284cc36766b969a15656b69cb613e3a0
whiteimage312 <g.andrei@gmail.com>
2025-08-03 12:54:29 +0300

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,10 @@
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-});
+  preview: {
+    port: 10000,
+    host: '0.0.0.0',
+  },
+})
